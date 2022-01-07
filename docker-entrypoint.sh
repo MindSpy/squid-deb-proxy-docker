37457cfd1bbb433394a9e02a36a012336b939762
pBorky <pborky@pborky.sk>
2022-01-07 22:31:00 +0100

    Add upstream proxy to the config.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,6 +9,19 @@ if [ "$1" = 'squid' ]; then
     pre_start
     post_start
 
+    if [ -n "$http_proxy"  ]; then
+      echo "Detected proxy setting: $http_proxy"
+      proxy_host=$(echo $http_proxy | sed 's/\(http[:][/][/]\)\?\([^:/]\+\)\([:]\([0-9]\+\)[/]\?\)\?/\2/g')
+      proxy_port=$(echo $http_proxy | sed 's/\(http[:][/][/]\)\?\([^:/]\+\)\([:]\([0-9]\+\)[/]\?\)\?/\4/g')
+      tee -a /etc/squid-deb-proxy/squid-deb-proxy.conf <<-EOF
+cache_peer $proxy_host parent $proxy_port 0 no-query default
+acl all src all
+http_access allow all
+never_direct allow all
+EOF
+
+    fi
+
     if [ "$USE_ACL" = "0" ]; then
         echo "[WARN] No mirror acl active"
         sed -i '/to_archive_mirrors/c\# No mirror acl active.' /etc/squid-deb-proxy/squid-deb-proxy.conf
