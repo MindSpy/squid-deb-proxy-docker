71578280b0391fe6d2d72c9d16f06e0f7fb661e7
Peter Boráros <pborky@pborky.sk>
2022-01-09 14:11:42 +0000

    use same buider also for DIRTY  and export env

--- bake.sh
+++ bake.sh
@@ -23,12 +23,13 @@ for arg in "$@"; do
         printf "\t--targets=[list of targets]  - list of targets to build\n\n"
         printf "\tNote: In case the local git contains changes only a \"dev\" target is built\n"
         printf "\t      for a single arch \"linux/386\" with docker image tag of \"dev-DIRTY\".\n"
-        printf "\t      A default docker builder is used for this purpose.\n"
         exit 
         ;;
     esac
 done
 
+# source and export env variables
+set -a
 test -e $env_file && source $env_file
 
 set -ex
@@ -47,18 +48,16 @@ if ! docker buildx ls | grep ^$BUILDER_NAME -c > /dev/null 2>&1; then
     docker buildx inspect --bootstrap
 fi
 
+# override env variables
+BUILD_DATE="$(date +%Y%m%d)"
 if [ -n "$(git status -s)" ]; then
   GIT_BRANCH=DIRTY 
 else 
   GIT_BRANCH=$(git rev-parse --short HEAD)
 fi
 
-BUILD_DATE="$(date +%Y%m%d)"
-
-BAKE_ARGS="-f env.hcl -f docker-bake.hcl"
-
 if [ -z "${GIT_BRANCH}" -o "${GIT_BRANCH}" == "DIRTY" ]; then
-  BAKE_ARGS="$BAKE_ARGS --progress=plain --builder default --set *.output=type=image --set *.platform=linux/386 dev"
+  BAKE_ARGS="$BAKE_ARGS --progress=plain --load --set *.platform=linux/386 dev"
 else
   BAKE_ARGS="$BAKE_ARGS --push $targets"
 fi
