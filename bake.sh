c9fb6fec9114db64d6fbdabda6ca10993615b397
Peter Boráros <pborky@pborky.sk>
2022-01-09 15:15:58 +0000

    change baker args

--- bake.sh
+++ bake.sh
@@ -2,24 +2,27 @@
 
 env_file=env.hcl
 BUILDER_NAME=multiarch
-
+BAKE_ARGS=""
 for arg in "$@"; do
   case "$arg" in
     --env=*) 
         env_file="${arg#*=}"
         ;;
-    --builder-name=*) 
+    --builder=*) 
         BUILDER_NAME="${arg#*=}"
         ;;
     --targets=*) 
         targets="${arg#*=}"
+        ;;
+    --print | --load )
+        BAKE_ARGS="$BAKE_ARGS $arg"
         ;;
     *)
         test "${arg}" != "--help" -a "${arg}" != "-h"  && printf "ERROR: unexpected argument: \"$arg\"\n"
         printf "\nUsage: $(basename $0) [OPTS]\n"
         printf "\nwhere [OPTS] can be:\n"
         printf "\t--env=[env file]  -  name of env file to source, default: env.hcl\n"
-        printf "\t--builder-name=[name]  - name of builder to create (if not exists) and use, default: multiarch\n"
+        printf "\t--builder=[name]  - name of builder to create (if not exists) and use, default: multiarch\n"
         printf "\t--targets=[list of targets]  - list of targets to build\n\n"
         printf "\tNote: In case the local git contains changes only a \"dev\" target is built\n"
         printf "\t      for a single arch \"linux/386\" with docker image tag of \"dev-DIRTY\".\n"
@@ -57,9 +60,9 @@ else
 fi
 
 if [ -z "${GIT_BRANCH}" -o "${GIT_BRANCH}" == "DIRTY" ]; then
-  BAKE_ARGS="$BAKE_ARGS --progress=plain --load --set *.platform=linux/386 dev"
+  BAKE_ARGS="$BAKE_ARGS --progress=plain --load --set *.platform=linux/amd64 dev"
 else
   BAKE_ARGS="$BAKE_ARGS --push $targets"
 fi
 
-docker buildx bake $BAKE_ARGS
+docker buildx bake -f docker-bake.hcl $BAKE_ARGS
