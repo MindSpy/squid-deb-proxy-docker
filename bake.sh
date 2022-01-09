0b376b4998b8d07df482e231d0d043c89c4009e9
Peter Boráros <pborky@pborky.sk>
2022-01-09 13:41:30 +0000

    add command line options

--- bake.sh
+++ bake.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-test -e env.hcl && source env.hcl
-
+env_file=env.hcl
 BUILDER_NAME=multiarch
-DRIVER_OPTS=""
-if [ -n "${PKG_PROXY_NET}" ]; then
-    DRIVER_OPTS="--driver-opt network=${PKG_PROXY_NET}"
-fi
+
+for arg in "$@"; do
+  case "$arg" in
+    --env=*) 
+        env_file="${arg#*=}"
+        ;;
+    --builder-name=*) 
+        BUILDER_NAME="${arg#*=}"
+        ;;
+    --targets=*) 
+        targets="${arg#*=}"
+        ;;
+    *)
+        test "${arg}" != "--help" -a "${arg}" != "-h"  && printf "ERROR: unexpected argument: \"$arg\"\n"
+        printf "\nUsage: $(basename $0) [OPTS]\n"
+        printf "\nwhere [OPTS] can be:\n"
+        printf "\t--env=[env file]  -  name of env file to source, default: env.hcl\n"
+        printf "\t--builder-name=[name]  - name of builder to create (if not exists) and use, default: multiarch\n"
+        printf "\t--targets=[list of targets]  - list of targets to build\n\n"
+        printf "\tNote: In case the local git contains changes only a \"dev\" target is built\n"
+        printf "\t      for a single arch \"linux/386\" with docker image tag of \"dev-DIRTY\".\n"
+        printf "\t      A default docker builder is used for this purpose.\n"
+        exit 
+        ;;
+    esac
+done
+
+test -e $env_file && source $env_file
 
 set -ex
 
@@ -15,10 +38,13 @@ if ! docker buildx ls | grep ^$BUILDER_NAME -c > /dev/null 2>&1; then
     docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
     docker run --privileged --rm tonistiigi/binfmt --install all
 
-    #docker buildx rm $BUILDER_NAME
+    DRIVER_OPTS=""
+    if [ -n "${PKG_PROXY_NET}" ]; then
+        DRIVER_OPTS="--driver-opt network=${PKG_PROXY_NET}"
+    fi
+
     docker buildx create --name $BUILDER_NAME --use --driver docker-container $DRIVER_OPTS
     docker buildx inspect --bootstrap
-
 fi
 
 if [ -n "$(git status -s)" ]; then
@@ -38,5 +64,3 @@ else
 fi
 
 docker buildx bake $BAKE_ARGS
-
-# docker buildx rm multiarch
